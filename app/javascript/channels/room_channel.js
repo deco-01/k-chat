a07ce54473a7be5502762d2b7e0611e294f57f1b
deco-01 <ex.k.futatunohoshino@gmail.com>
2023-07-13 01:24:43 +0900

    チャット機能の実装

---
+++ app/javascript/channels/room_channel.js
@@ -0,0 +1,21 @@
+import consumer from "./consumer"
+
+const appRoom = consumer.subscriptions.create("RoomChannel", {
+
+  received(data) {
+    const messages = document.getElementById('messages');
+    messages.insertAdjacentHTML('beforeend', data['message']);
+  },
+
+  speak: function(message) {
+    return this.perform('speak', {message: message});
+  }
+});
+
+window.addEventListener("keypress", function(e) {
+  if (e.keyCode === 13) {
+    appRoom.speak(e.target.value);
+    e.target.value = '';
+    e.preventDefault();
+  }
+})
