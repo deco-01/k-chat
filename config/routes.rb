c20594c0af1323fd43fcb59e58e5dbf5fbf2357b
deco-01 <ex.k.futatunohoshino@gmail.com>
2023-07-17 02:43:41 +0900

    ボタン装飾

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'rooms#show'
   resource :user, except: [:new, :create, :destroy]
+  get '/room', to: 'rooms#show', as: 'room'
+  get '/user', to: 'users#show', as: 'user_profile'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
